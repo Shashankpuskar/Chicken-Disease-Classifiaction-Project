c1c277e4fcaf1d420f435c94380c5ed9532085b3
shashankpuskr <shashankkrkr123@gmail.com>
2025-02-17 04:37:36 +0530

    prepare base model completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,59 +1,16 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
 from box import ConfigBox
 
-# This code contains some error which will be resolved later
-# class ConfigurationManager:
-#     def __init__(self,
-#                 config_filepath = CONFIG_FILE_PATH,
-#                 params_filepath = PARAMS_FILE_PATH):
-        
-#         self.config = config_filepath
-#         self.params = params_filepath
-        
-#         create_directories([self.config.artifacts_root])
-        
-        
-#     def get_data_ingestion_config(self) -> DataIngestionConfig:
-#         config = self.config.data_ingestion
-        
-#         create_directories([config.root_dir])
-        
-#         data_ingestion_config = DataIngestionConfig(
-#             root_dir=config.root_dir,
-#             source_URL=config.source_URL,
-#             local_data_file=config.local_data_file,
-#             unzip_dir=config.unzip_dir
-#         )
-        
-#         return data_ingestion_config
 
-# This is corrected code
 class ConfigurationManager:
-    def __init__(self, 
-                 config_filepath: Path = CONFIG_FILE_PATH, 
-                 params_filepath: Path = PARAMS_FILE_PATH):
-        
-        # Ensure config_filepath is a Path object
-        self.config_filepath = Path(config_filepath)
-
-        # Read and parse the YAML file
-        self.config = self.read_yaml(self.config_filepath)
-        self.params = params_filepath  # Handle params similarly if needed
-
-        # Ensure artifacts_root exists before creating directories
-        if hasattr(self.config, "artifacts_root"):
-            create_directories([self.config.artifacts_root])
-        else:
-            raise AttributeError("Missing 'artifacts_root' in config file")
-        
-    @staticmethod
-    def read_yaml(path_to_yaml: Path) -> ConfigBox:
-        """Reads a YAML file and returns a ConfigBox object."""
-        with open(path_to_yaml, "r") as yaml_file:
-            content = yaml.safe_load(yaml_file) or {}
-        return ConfigBox(content)  # Allows dot notation access
+    def __init__(self,
+                config_filepath = CONFIG_FILE_PATH,
+                params_filepath = PARAMS_FILE_PATH):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        create_directories([self.config.artifacts_root])
         
         
     def get_data_ingestion_config(self) -> DataIngestionConfig:
@@ -70,3 +27,23 @@ class ConfigurationManager:
         )
         
         return data_ingestion_config
+
+
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+        
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir = Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        
+        return prepare_base_model_config
